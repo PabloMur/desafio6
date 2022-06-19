3d08e25f8bafdc824603e6aa07d8b9001de1b290
Pablo <polillomurillo@gmail.com>
2022-06-19 20:55:16 -0300

    lisd

--- client/src/components/starComp.ts
+++ client/src/components/starComp.ts
@@ -47,9 +47,9 @@ class StarComp extends HTMLElement {
     this.shadow.appendChild(style);
   }
   sync() {
-    const ganaste = require("../img/ganaste.png");
-    const perdiste = require("../img/perdiste.png");
-    const empataste = require("../img/empataste.png");
+    const ganaste = require("./ganaste.png") as any;
+    const perdiste = require("./perdiste.png") as any;
+    const empataste = require("./empataste.png") as any;
 
     const lastState = state.getState();
 
